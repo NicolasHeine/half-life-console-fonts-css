0e3b85f1167a18a3e5af0325d3e0901ab0d824b2
Nicolas Heine <nicolas.heine11@gmail.com>
2019-02-26 11:48:28 +1100

    Add Half-life console font 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+Array.prototype.forEach.call(document.getElementsByClassName("h-l1"), function(el){
+	if(el.childNodes.length === 1){
+		var text = el.textContent;
+		var result = '<span class="h-l1__accessible">'+text+'</span>';
+		var words = text.split(' ');
+		for(word in words){
+			result += '<span class="h-l1__word">';
+			var letters = words[word].split('');
+			for(letter in letters){
+				if(letters[letter] === '"'){
+					result += "<b class='h-l1:"+letters[letter]+"'>"+letters[letter]+"</b>";
+				}else{
+					result += '<b class="h-l1:'+letters[letter]+'">'+letters[letter]+'</b>';
+				}
+			}
+			result += '</span>';
+		}
+		el.innerHTML = result;
+	}
+});
+
 Array.prototype.forEach.call(document.getElementsByClassName("h-l2"), function(el){
 	if(el.childNodes.length === 1){
 		var text = el.textContent;
@@ -7,7 +28,11 @@ Array.prototype.forEach.call(document.getElementsByClassName("h-l2"), function(e
 			result += '<span class="h-l2__word">';
 			var letters = words[word].split('');
 			for(letter in letters){
-				result += '<b class="h-l2:'+letters[letter]+'">'+letters[letter]+'</b>';
+				if(letters[letter] === '"'){
+					result += "<b class='h-l2:"+letters[letter]+"'>"+letters[letter]+"</b>";
+				}else{
+					result += '<b class="h-l2:'+letters[letter]+'">'+letters[letter]+'</b>';
+				}
 			}
 			result += '</span>';
 		}
